2f8fa4f5e0366d517e24e15cfe2b371bcb07ba0b
Hzk <P76073016@ncku.edu.tw>
2018-10-20 10:20:59 +0800

    Initial commit

---
+++ src/test/Text.java
@@ -0,0 +1,20 @@
+package test;
+
+class Text
+{
+	float X,Y,fs,xscale,height,space,width;
+	String unicode;
+
+	Text(float _X,float _Y,float _fs,float _xscale,float _height,float _space,float _width , String _u )
+	{
+		X = _X;
+		Y = _Y;
+		fs = _fs;
+		xscale = _xscale;
+		height = _height;
+		space = _space;
+		width = _width;
+		unicode = _u;
+		
+	}
+};
