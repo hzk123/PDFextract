1da3cd9839c07248247b4e08cc71d0255295e35a
Kevin <ProFatXuanAll@gmail.com>
2018-10-31 00:42:41 +0800

    public release version

---
+++ src/Text.java
@@ -0,0 +1,15 @@
+public class Text {
+	float X, Y, fs, xscale, height, space, width;
+	String unicode;
+
+	Text(float X, float Y, float fs, float xscale, float height, float space, float width, String u) {
+		this.X = X;
+		this.Y = Y;
+		this.fs = fs;
+		this.xscale = xscale;
+		this.height = height;
+		this.space = space;
+		this.width = width;
+		this.unicode = u;
+	}
+};
